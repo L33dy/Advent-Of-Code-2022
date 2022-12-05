c6045c1e3aa18185acd97a65f57aff908b791f99
L33dy <zbynekzidlicky@gmail.com>
2022-12-05 15:28:53 +0100

    Day2 added, unknown mistake

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -9,7 +10,7 @@ namespace Advent_Of_Code_2022
     {
         public static void Main(string[] args)
         {
-            Day1();
+            Day2();
         }
 
         private static void Day1()
@@ -30,8 +31,6 @@ namespace Advent_Of_Code_2022
                 {
                     if (curr == " ")
                     {
-                        Console.WriteLine(" NEW ELF ");
-
                         if (mostCalories < currentCalories)
                         {
                             mostCalories = currentCalories;
@@ -56,6 +55,113 @@ namespace Advent_Of_Code_2022
             }
 
             Console.WriteLine("Most Calories: " + mostCalories);
+        }
+
+        private static void Day2()
+        {
+            /* Score for the shape
+             * ROCK - 1
+             * PAPER - 2
+             * SCISSORS - 3
+             */
+
+            /* Score for the outcome
+             * LOST - 0
+             * DRAW - 3
+             * WIN - 6
+             */
+
+            var input = File.ReadAllLines("inputs/day2.txt");
+            var totalScore = 0;
+
+            Dictionary<char, int> opponent = new Dictionary<char, int>
+            {
+                { 'A', 1 },
+                { 'B', 2 },
+                { 'C', 3 }
+            };
+
+            Dictionary<char, int> me = new Dictionary<char, int>
+            {
+                { 'X', 1 },
+                { 'Y', 2 },
+                { 'Z', 3 }
+            };
+
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var splited = input[i];
+                var opponentShape = 0;
+                var myShape = 0;
+                int gameState;
+
+                foreach (var c in splited)
+                {
+                    if (c == ' ') continue;
+
+                    if (opponentShape == 0)
+                    {
+                        opponentShape = SearchInDictionary(c, opponent);
+                    }
+
+                    if (myShape == 0)
+                    {
+                        myShape = SearchInDictionary(c, me);
+                    }
+
+                    Console.WriteLine(c);
+                }
+
+                gameState = DetermineState(opponentShape, myShape);
+                totalScore += (myShape + gameState);
+                
+                Console.WriteLine("Opponent shape: " + opponentShape);
+                Console.WriteLine("My shape: " + myShape);
+                Console.WriteLine("Game State: " + gameState);
+                Console.WriteLine("Total Score: " + totalScore);
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine(totalScore);
+
+
+            int SearchInDictionary(char c, Dictionary<char, int> dict)
+            {
+                foreach (var ch in dict)
+                {
+                    if (ch.Key == c)
+                    {
+                        return ch.Value;
+                    }
+                }
+
+                return 0;
+            }
+
+            int DetermineState(int opponentValue, int meValue)
+            {
+                int lost = 0;
+                int draw = 3;
+                int win = 6;
+
+                if (opponentValue == 1 && meValue == 2 || opponentValue == 2 && meValue == 3 ||
+                    opponentValue == 3 && meValue == 2)
+                {
+                    return win;
+                }
+
+                if (opponentValue == 1 && meValue == 1 || opponentValue == 2 && meValue == 2 ||
+                    opponentValue == 3 && meValue == 3)
+                {
+                    return draw;
+                }
+
+                return lost;
+            }
+            
+            Console.ReadLine();
         }
     }
 }
